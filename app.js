72c376b6bf5cd61495c0c8d18a2b9fbda223d97f
Sarthak Jain <sarthak1280.be21@chitkara.edu.in>
2022-10-29 01:24:40 +0530

    gitignore file added

--- app.js
+++ app.js
@@ -128,6 +128,11 @@ app.post('/delete', (req, res) => {
     }
 });
 
-app.listen(3000, function () {
+let port = process.env.PORT;
+if (port === null || port == "") {
+    port = 3000;
+}
+
+app.listen(port, function () {
     console.log('Server started on Port 3000');
 });
